697adb942dff0bcef9e047d55eef676a9be71c68
angie angulo <angieangulo277@gmail.com>
2024-10-01 15:29:40 -0500

    Creacion del componente footer

---
+++ Farmacia/src/Componentes/Footer/FooterComponente.jsx
@@ -0,0 +1,17 @@
+
+const FooterComponente = () => {
+  return (
+    <div className="container">
+        <p>&copy; 2024 La Buena Salud.</p>
+        <address>
+        <p><strong>Farmacia La Buena Salud</strong></p>
+        <p>Cra 52 # 50 - 70, Medellín, Colombia</p>
+        <p>Teléfono: <a href="tel:+57355555555">3555555</a></p>
+        <p>Correo electrónico: <a href="mailto:info@FarmaciaLabuenaSalud.com">info@FarmaciaLabuenaSalud.com</a></p>
+      </address>
+    </div>
+  )
+}
+
+export default FooterComponente
+
