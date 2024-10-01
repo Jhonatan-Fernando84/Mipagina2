893387d94584627f882e5338bb1c75d5007ab8fe
angie angulo <angieangulo277@gmail.com>
2024-10-01 15:35:00 -0500

    Creacion del componente servicio

---
+++ Farmacia/src/Componentes/Servicio/Servicios.jsx
@@ -0,0 +1,15 @@
+import MapaGoogle from "../Mapa/MapaComponte"
+
+
+const Servicios = () => {
+  return (
+    <>
+        <h2>Servicios</h2>
+        <b>Tenemos servicio de consulta  médica, odontológica y de enfermería tambien contamos con 30% de descuento en todos los productos todos los lunes de 6am a 9am.</b>
+
+       <MapaGoogle/>
+    </>
+  )
+}
+
+export default Servicios
