7706ca2a6d67380b268b476427810eef4aaf043d
Stiven-Valencia <brayan12.es@gmail.com>
2024-10-04 09:23:02 -0600

    BREAKING CHANGE: Se agregan nombres en el footer

--- Farmacia/src/Componentes/Footer/FooterComponente.jsx
+++ Farmacia/src/Componentes/Footer/FooterComponente.jsx
@@ -5,6 +5,7 @@ const FooterComponente = () => {
         <p>&copy; 2024 La Buena Salud.</p>
         <address>
         <p><strong>Farmacia La Buena Salud</strong></p>
+        <p><strong>Derechos reservados a AngieAngulo JohnGarcia BrayhanOrrego JhonatanAriza</strong></p>
         <p>Cra 52 # 50 - 70, Medellín, Colombia</p>
         <p>Teléfono: <a href="tel:+57355555555">3555555</a></p>
         <p>Correo electrónico: <a href="mailto:info@FarmaciaLabuenaSalud.com">info@FarmaciaLabuenaSalud.com</a></p>
