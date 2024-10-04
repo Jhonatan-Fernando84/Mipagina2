20b5ee9ed7d1e82ff409db16026ccb95cd37a220
Stiven-Valencia <brayan12.es@gmail.com>
2024-10-04 14:27:19 -0600

    se cambian titulo y precios mal agregados

--- Farmacia/src/Componentes/Footer/FooterComponente.jsx
+++ Farmacia/src/Componentes/Footer/FooterComponente.jsx
@@ -4,7 +4,6 @@ const FooterComponente = () => {
     <div className="container">
         <p>&copy; 2024 La Buena Salud.</p>
         <address>
-        <p><strong>Farmacia La Buena Salud</strong></p>
         <p><strong>Derechos reservados a AngieAngulo JohnGarcia BrayhanOrrego JhonatanAriza</strong></p>
         <p>Cra 52 # 50 - 70, Medellín, Colombia</p>
         <p>Teléfono: <a href="tel:+57355555555">3555555</a></p>
